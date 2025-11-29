211b694e6c1854e9f05f90efb0f5974543d7f697
mrdigitalvip-maker <mrdigitalvip@gmail.com>
2025-11-29 18:38:31 -0300

    Delete page.jsx

--- page.jsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>BillionMind Global</h1>
-      <p>App carregado com sucesso.</p>
-      <p>Agora vamos importar seu HTML oficial.</p>
-    </div>
-  );
-}
