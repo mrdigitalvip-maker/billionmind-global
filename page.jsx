bc7b7a374ed8b322bb0c774a74e5eb52222f5d02
mrdigitalvip-maker <mrdigitalvip@gmail.com>
2025-11-29 18:35:37 -0300

    Create page.jsx

---
+++ page.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>BillionMind Global</h1>
+      <p>App carregado com sucesso.</p>
+      <p>Agora vamos importar seu HTML oficial.</p>
+    </div>
+  );
+}
