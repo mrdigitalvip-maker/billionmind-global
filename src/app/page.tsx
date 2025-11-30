278b7f1b52f685d4db2adcfe9eb5212de62fbe1f
mrdigitalvip-maker <mrdigitalvip@gmail.com>
2025-11-30 12:11:24 -0300

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div style={{
+      padding: "40px",
+      fontFamily: "Arial",
+      color: "white",
+      background: "black",
+      minHeight: "100vh"
+    }}>
+      <h1>BillionMind Global</h1>
+      <p>App iniciado com sucesso.</p>
+      <p>Agora podemos criar as páginas internas.</p>
+    </div>
+  );
+}
